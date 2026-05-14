49bb0e8001c9893ae24a04f88a1c0d9951e64cec
Tran Thao Ngan <tranthaongan7epct@gmail.com>
2026-05-14 18:42:39 +0700

    update AI, save AI explain in Note, improve to-do

--- src/services/endpoints/flashcard.ts
+++ src/services/endpoints/flashcard.ts
@@ -11,7 +11,6 @@ import type {
   DeleteMultipleCardsResponse,
   UpdateMultipleCardsRequest,
   UpdateMultipleCardsResponse,
-  GenerateFlashcardsFromNoteRequest,
   GenerateFlashcardsFromWebRequest,
   GenerateFlashcardsFromNoteResponse,
   CreateFlashcardResponse,
